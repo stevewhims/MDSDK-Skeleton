814ff6442d0b6a812aa09aedd3b1a8a40186159a
Steven White <stwhi@microsoft.com>
2020-05-06 01:14:19 -0700

    .

--- MDSDK-Skeleton/Program.cs
+++ MDSDK-Skeleton/Program.cs
@@ -48,48 +48,13 @@ namespace MDSDKDerived
             this.RegisterLog(this.exampleLog);
             this.exampleLog.Add("Example message.");
 
-            var processStartInfo = new ProcessStartInfo("cmd");
-            processStartInfo.CreateNoWindow = true;
-            processStartInfo.RedirectStandardInput = true;
-            processStartInfo.RedirectStandardOutput = true;
-            processStartInfo.UseShellExecute = false;
-            processStartInfo.WorkingDirectory = $"{ProgramBase.MyContentReposFolderDirectoryInfo.FullName}\\{ProgramBase.ContentRepo}";
-
-            using (var createBranchProcess = Process.Start(processStartInfo))
-            {
-                createBranchProcess.StandardInput.WriteLine("git pull -p");
-                createBranchProcess.StandardInput.WriteLine("git checkout -b stwhi-master/refactor-mega-tables-3 origin/master");
-                //createBranchProcess.StandardInput.WriteLine("git push --set-upstream origin stwhi-master/refactor-mega-tables-3");
-                createBranchProcess.StandardInput.WriteLine("git status");
-                createBranchProcess.StandardInput.Close();
-                createBranchProcess.WaitForExit();
-                ProgramBase.ConsoleWrite(createBranchProcess.StandardOutput.ReadToEnd(), ConsoleWriteStyle.OutputFromAnotherProcess);
-                createBranchProcess.WaitForExit();
-            }
-
-            using (var pushProcess = Process.Start(processStartInfo))
-            {
-                pushProcess.StandardInput.WriteLine("git add .");
-                pushProcess.StandardInput.WriteLine("git commit -a -m \"commit message\"");
-                pushProcess.StandardInput.WriteLine("git push");
-                pushProcess.StandardInput.WriteLine("git status");
-                pushProcess.StandardInput.Close();
-                pushProcess.WaitForExit();
-                ProgramBase.ConsoleWrite(pushProcess.StandardOutput.ReadToEnd(), ConsoleWriteStyle.OutputFromAnotherProcess);
-                pushProcess.WaitForExit();
-            }
-
-            using (var deleteBranchProcess = Process.Start(processStartInfo))
-            {
-                deleteBranchProcess.StandardInput.WriteLine("git checkout master");
-                deleteBranchProcess.StandardInput.WriteLine("git branch -D stwhi-master/refactor-mega-tables-3");
-                deleteBranchProcess.StandardInput.WriteLine("git pull -p");
-                deleteBranchProcess.StandardInput.WriteLine("git status");
-                deleteBranchProcess.StandardInput.Close();
-                deleteBranchProcess.WaitForExit();
-                ProgramBase.ConsoleWrite(deleteBranchProcess.StandardOutput.ReadToEnd(), ConsoleWriteStyle.OutputFromAnotherProcess);
-                deleteBranchProcess.WaitForExit();
-            }
+            // Specifics are in configuration.txt.
+            //GitProcess.CreatePersonalBranch(true);
+            // Edit and save files here.
+            //GitProcess.StageAndCommit();
+            // You can now create your pull-request.
+            // Delete personal branch now if you're sure you're done with it.
+            //GitProcess.DeletePersonalBranch(true);
 
             //this.uniqueKeyMap = this.LoadUniqueKeyMap("uniqueKeyMap.txt");
             //this.nonUniqueKeyMap = this.LoadNonUniqueKeyMap("nonUniqueKeyMap.txt");
